401c330ca69c6f40cc96c53a7c8f2154aa509473
black32h <blackspetya@gmail.com>
2025-01-14 20:58:46 +0200

    Update DateRangeRequest.java

--- src/main/java/com/example/bablo/DateRangeRequest.java
+++ src/main/java/com/example/bablo/DateRangeRequest.java
@@ -1,4 +1,40 @@
 package com.example.bablo;
 
-public class DateRangeRequest {
+// Клас для представлення запиту з діапазоном дат та валютою
+class DateRangeRequest {
+
+    private String startDate; // Стартова дата
+    private String endDate; // Кінцева дата
+    private String currency; // Валюта
+
+    // Геттери та сеттери для доступу до полів
+    // Геттер для отримання стартової дати
+    public String getStartDate() {
+        return startDate;
+    }
+
+    // Сеттер для встановлення стартової дати
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    // Геттер для отримання кінцевої дати
+    public String getEndDate() {
+        return endDate;
+    }
+
+    // Сеттер для встановлення кінцевої дати
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    // Геттер для отримання валюти
+    public String getCurrency() {
+        return currency;
+    }
+
+    // Сеттер для встановлення валюти
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
 }
