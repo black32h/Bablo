ea2b2319cb721dda4f0da0e4657542dc052d4a0f
black32h <blackspetya@gmail.com>
2025-01-14 20:58:18 +0200

    Update CurrencyServiceApplication.java

--- src/main/java/com/example/bablo/CurrencyServiceApplication.java
+++ src/main/java/com/example/bablo/CurrencyServiceApplication.java
@@ -1,16 +1,20 @@
+package com.example.bablo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// Анотація для вказівки, що це головний клас для запуску Spring Boot додатку
 @SpringBootApplication
+// Анотація для активації Feign клієнтів для взаємодії з іншими сервісами через HTTP
 @EnableFeignClients
 public class CurrencyServiceApplication {
+    // Головний метод для запуску Spring Boot додатку
     public static void main(String[] args) {
+        // Запуск Spring Boot додатку
         SpringApplication.run(CurrencyServiceApplication.class, args);
     }
 }
-
-
 
 
 
